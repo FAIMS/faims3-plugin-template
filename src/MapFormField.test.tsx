0477a192e1662166f91670a3cb91ee555988b270
Steve Cassidy <steve.cassidy@mq.edu.au>
2021-10-19 17:15:06 +1100

    add a couple of simple tests
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

---
+++ src/MapFormField.test.tsx
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen, fireEvent, findByText } from '@testing-library/react'
+import React from 'react'
+// import { Field, Form, Formik } from 'formik';
+import { MapFormField } from '.'
+
+it('renders as a button', () => {
+  const props = {
+    value: {},
+    name: 'point',
+    onChange: (x: any) => x,
+    onBlur: (x: any) => x
+  }
+  render(<MapFormField field={props} featureType='Point' />)
+  expect(screen.getByText('Get Point'))
+  expect(screen.getByRole('button'))
+})
+
+it('creates a map when the button is pressed', async () => {
+  const props = {
+    value: {},
+    name: 'point',
+    onChange: jest.fn(),
+    onBlur: jest.fn()
+  }
+  window.scrollTo = jest.fn()
+  render(<MapFormField field={props} featureType='Point' />)
+  fireEvent.click(screen.getByRole('button'))
+  expect(document.querySelector('.ol-viewport'))
+})

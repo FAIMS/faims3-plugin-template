c892612770ffc24654efa92fe1d51acc0e6919af
Steve Cassidy <steve.cassidy@mq.edu.au>
2021-10-20 10:55:53 +1100

    fix lint errors
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -6,12 +6,9 @@ declare module '*.css' {
   const content: { [className: string]: string }
   export default content
 }
-
-interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
-
 declare module '*.svg' {
   const svgUrl: string
-  const svgComponent: SvgrComponent
+  const svgComponent: React.StatelessComponent<React.SVGAttributes<SVGElement>>
   export default svgUrl
   export { svgComponent as ReactComponent }
 }

46df48c45da6c193e79bde836bd8bc37fd45c075
Steve Cassidy <steve.cassidy@mq.edu.au>
2022-01-07 10:51:28 +1100

    Add working export of settings for field config

--- src/MapFormFieldSettings.tsx
+++ src/MapFormFieldSettings.tsx
@@ -1,25 +1,3 @@
-export const MapFieldUISpec = {
-  'component-namespace': 'mapping-plugin', // this says what web component to use to render/acquire value from
-  'component-name': 'MapFormField',
-  'type-returned': 'faims-core::JSON', // matches a type in the Project Model
-  'component-parameters': {
-    name: 'map-field',
-    id: 'map-field'
-  },
-  validationSchema: [
-    ['yup.object'],
-    ['yup.nullable'],
-    [
-      'yup.shape',
-      {
-        type: [['yup.string'], ['yup.required']],
-        features: [['yup.object'], ['yup.nullable']]
-      }
-    ]
-  ],
-  initialValue: {}
-}
-
 // should really be imported from the main project
 interface ProjectUIModel {
   _id?: string // optional as we may want to include the raw json in places
@@ -43,39 +21,93 @@ interface ProjectUIModel {
   visible_types: string[]
 }
 
+export const MapFieldUISpec = {
+  'component-namespace': 'mapping-plugin', // this says what web component to use to render/acquire value from
+  'component-name': 'MapFormField',
+  'type-returned': 'faims-core::JSON', // matches a type in the Project Model
+  'component-parameters': {
+    name: 'radio-group-field',
+    id: 'radio-group-field',
+    variant: 'outlined',
+    required: false,
+    featureType: 'Point',
+    zoom: 12,
+    label: '',
+    FormLabelProps: {
+      children: 'Select from Map'
+    }
+  },
+  validationSchema: [['yup.string']],
+  initialValue: '1'
+}
+
 export const MapFieldUISetting = (defaultSetting: ProjectUIModel) => {
+  console.log('In MapField Settings')
+
   const newuiSetting = Object.assign({}, defaultSetting)
+
+  newuiSetting['fields']['featureType'] = {
+    'component-namespace': 'faims-custom', // this says what web component to use to render/acquire value from
+    'component-name': 'Select',
+    'type-returned': 'faims-core::String', // matches a type in the Project Model
+    'component-parameters': {
+      fullWidth: true,
+      helperText: '',
+      variant: 'outlined',
+      required: true,
+      select: true,
+      InputProps: {},
+      SelectProps: {},
+      ElementProps: {
+        options: [
+          {
+            value: 'Point',
+            label: 'Point'
+          },
+          {
+            value: 'Polygon',
+            label: 'Polygon'
+          },
+          {
+            value: 'LineString',
+            label: 'LineString'
+          }
+        ]
+      },
+      InputLabelProps: {
+        label: 'Select Feature Type'
+      }
+    },
+    validationSchema: [['yup.string']],
+    initialValue: 'Point'
+  }
+
+  newuiSetting['fields']['zoom'] = {
+    'component-namespace': 'formik-material-ui', // this says what web component to use to render/acquire value from
+    'component-name': 'TextField',
+    'type-returned': 'faims-core::Integer', // matches a type in the Project Model
+    'component-parameters': {
+      fullWidth: true,
+      helperText: 'Zoom Level',
+      required: false,
+      InputProps: {
+        type: 'number' // must be a valid html type
+      },
+      SelectProps: {},
+      FormHelperTextProps: {}
+    },
+    validationSchema: [['yup.string']],
+    initialValue: 12
+  }
+
+  newuiSetting['views']['FormParamater']['fields'] = ['featureType', 'zoom']
+
   newuiSetting['viewsets'] = {
     settings: {
-      views: [],
+      views: ['FormLabelProps', 'FormParamater'],
       label: 'settings'
     }
   }
+  console.log('NEW SETTINGS', newuiSetting)
   return newuiSetting
 }
-
-
-// const MapFormFieldUISetting = {
-//   namespace: 'faims-custom',
-//   componentName: 'TemplatedStringField',
-//   type_return: 'faims-core::String',
-//   required: true,
-//   validationSchema: [['yup.string'], ['yup.required']],
-//   type: 'text',
-//   template: 'αβγ {{str-field}}-{{basic-autoincrementer-field}}'
-// }
-
-// registerComponent(
-//   'faims-custom',
-//   'TemplatedStringField',
-//   setupComponentProperties(
-//     'Map Input Field',
-//     'Input Geo Data via a map',
-//     'Maps',
-//     MapFormField,
-//     MapFormFieldUISetting /* FAIMSUISpec */,
-//     MapFormFieldSetting /* Array<FAIMSUISpec> */,
-//     MapFormFieldComponentSetting /* builder_component */
-//     /* FAIMSBuilderIcon */
-//   )
-// )
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,2 @@
 export { MapFormField } from './MapFormField'
+export { MapFieldUISpec, MapFieldUISetting } from './MapFormFieldSettings'
